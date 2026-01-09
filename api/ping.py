8c459ca3b292172209a2bfb0e63ecd020203b184
Giorgio223 <103820829+Giorgio223@users.noreply.github.com>
2026-01-09 02:35:08 +0100

    Delete api/ping.py

--- api/ping.py
+++
@@ -1,8 +0,0 @@
-import json
-
-def handler(request):
-    return {
-        "statusCode": 200,
-        "headers": {"Content-Type": "application/json", "Cache-Control": "no-store"},
-        "body": json.dumps({"ok": True, "route": "/api/ping"})
-    }
